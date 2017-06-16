5fec279371e46c92a7891ef77e771577ccc2cf34
nevillealee <nevillealee@gmail.com>
2017-06-16 18:40:07 +0000

    modal error fix

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,6 @@ class QuotesController < ApplicationController
         @quote = Quote.create(quote_params)
         
         if @quote.invalid?
-            
             flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
         end
         redirect_to root_path
